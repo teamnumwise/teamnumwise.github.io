a93805759da5a5f8abe2e93a3262880fa436ce5d
usari14 <udogar583@gmail.com>
2026-04-13 15:59:04 +0500

    chore: configure next.config.ts for static export and GitHub Pages compatibility

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+  basePath: "", // Ensures assets are loaded from the root for your custom domain
   images: {
+    unoptimized: true, // GitHub Pages doesn't support Next.js default image optimization
     remotePatterns: [
       {
         protocol: "https",
